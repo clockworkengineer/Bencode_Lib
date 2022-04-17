9ba4847d0d463fe6d4a76b69edbbf2f9502753ef
clockworkengineer <robert_tizzard@hotmail.com>
2022-04-17 13:27:40 +0100

    Add extra tests for r-value reference on encode/decode.

--- tests/Bencode_tests_main.cpp
+++ tests/Bencode_tests_main.cpp
@@ -81,20 +81,20 @@ TEST_CASE("ISource (File interface).", "[Bencode][Decode][ISource]")
   }
   SECTION("Create FileSource with singlefile.torrent. and positioned on the correct first character", "[Bencode][Decode][ISource]")
   {
-    FileSource source { prefixTestDataPath(kSingleFileTorrent) };
+    FileSource source{prefixTestDataPath(kSingleFileTorrent)};
     REQUIRE_FALSE(!source.more());
     REQUIRE((char)source.current() == 'd');
   }
   SECTION("Create FileSource with singlefile.torrent and then check moveToNextByte positions to correct next character", "[Bencode][Decode][ISource]")
   {
-    FileSource source { prefixTestDataPath(kSingleFileTorrent) };
+    FileSource source{prefixTestDataPath(kSingleFileTorrent)};
     source.next();
     REQUIRE_FALSE(!source.more());
     REQUIRE((char)source.current() == '8');
   }
   SECTION("Create FileSource with singlefile.torrent move past last character, check it and the bytes moved.", "[Bencode][Decode][ISource]")
   {
-    FileSource source { prefixTestDataPath(kSingleFileTorrent) };
+    FileSource source{prefixTestDataPath(kSingleFileTorrent)};
     long length = 0;
     while (source.more())
     {
@@ -107,7 +107,7 @@ TEST_CASE("ISource (File interface).", "[Bencode][Decode][ISource]")
 }
 TEST_CASE("ISource (Buffer interface). Contains file singlefile.torrent.", "[Bencode][Decode][ISource]")
 {
-  std::string bencodedBuffer { readBencodedBytesFromFile(prefixTestDataPath(kSingleFileTorrent)) };
+  std::string bencodedBuffer{readBencodedBytesFromFile(prefixTestDataPath(kSingleFileTorrent))};
   SECTION("Create BufferSource.", "[Bencode][Decode][ISource]")
   {
     REQUIRE_NOTHROW(BufferSource(bencodedBuffer));
@@ -119,20 +119,20 @@ TEST_CASE("ISource (Buffer interface). Contains file singlefile.torrent.", "[Ben
   }
   SECTION("Create BufferSource with singlefile.torrent and that it is positioned on the correct first character", "[Bencode][Decode][ISource]")
   {
-    BufferSource source { bencodedBuffer };
+    BufferSource source{bencodedBuffer};
     REQUIRE_FALSE(!source.more());
     REQUIRE((char)source.current() == 'd');
   }
   SECTION("Create BufferSource with singlefile.torrent and then check moveToNextByte positions to correct next character", "[Bencode][Decode][ISource]")
   {
-    BufferSource source { bencodedBuffer };
+    BufferSource source{bencodedBuffer};
     source.next();
     REQUIRE_FALSE(!source.more());
     REQUIRE((char)source.current() == '8');
   }
   SECTION("Create BufferSource with singlefile.torrent move past last character, check it and the bytes moved.", "[Bencode][Decode][ISource]")
   {
-    BufferSource source { bencodedBuffer };
+    BufferSource source{bencodedBuffer};
     long length = 0;
     while (source.more())
     {
@@ -177,33 +177,33 @@ TEST_CASE("IDestination (File interface).", "[Bencode][Decode][ISource]")
   }
   SECTION("Create FileDestination when file already exists.", "[Bencode][Encode][IDesination]")
   {
-    FileDestination file { prefixTestDataPath(kGeneratedTorrentFile) };
+    FileDestination file{prefixTestDataPath(kGeneratedTorrentFile)};
     REQUIRE_NOTHROW(FileDestination(prefixTestDataPath(kGeneratedTorrentFile)));
   }
   SECTION("Create FileDestination and test file exists and should be empty.", "[Bencode][Encode][IDesination]")
   {
     std::filesystem::remove(prefixTestDataPath(kGeneratedTorrentFile));
-    FileDestination file { prefixTestDataPath(kGeneratedTorrentFile) };
+    FileDestination file{prefixTestDataPath(kGeneratedTorrentFile)};
     REQUIRE_FALSE(!std::filesystem::exists(prefixTestDataPath(kGeneratedTorrentFile)));
-    std::filesystem::path filePath { prefixTestDataPath(kGeneratedTorrentFile) };
+    std::filesystem::path filePath{prefixTestDataPath(kGeneratedTorrentFile)};
     REQUIRE(std::filesystem::file_size(filePath) == 0);
   }
   SECTION("Create FileDestination and add one character.", "[Bencode][Encode][IDesination]")
   {
     std::filesystem::remove(prefixTestDataPath(kGeneratedTorrentFile));
-    FileDestination file { prefixTestDataPath(kGeneratedTorrentFile) };
+    FileDestination file{prefixTestDataPath(kGeneratedTorrentFile)};
     file.add("i");
-    std::filesystem::path filePath { prefixTestDataPath(kGeneratedTorrentFile) };
+    std::filesystem::path filePath{prefixTestDataPath(kGeneratedTorrentFile)};
     REQUIRE(std::filesystem::file_size(filePath) == 1);
   }
   SECTION("Create FileDestination, add an encoded integer and check result.", "[Bencode][Encode][IDesination]")
   {
     std::filesystem::remove(prefixTestDataPath(kGeneratedTorrentFile));
-    FileDestination file { prefixTestDataPath(kGeneratedTorrentFile) };
+    FileDestination file{prefixTestDataPath(kGeneratedTorrentFile)};
     file.add("i65767e");
-    std::filesystem::path filePath { prefixTestDataPath(kGeneratedTorrentFile) };
+    std::filesystem::path filePath{prefixTestDataPath(kGeneratedTorrentFile)};
     REQUIRE(std::filesystem::file_size(filePath) == 7);
-    std::string expected  { readBencodedBytesFromFile(prefixTestDataPath(kGeneratedTorrentFile)) };
+    std::string expected{readBencodedBytesFromFile(prefixTestDataPath(kGeneratedTorrentFile))};
     REQUIRE(expected == "i65767e");
   }
 }
@@ -212,7 +212,7 @@ TEST_CASE("Use of BNode indexing operators", "[Bencode][BNode][Index]")
   Bencode bEncode;
   SECTION("Decode dictionary and check its components using indexing", "[Bencode][BNode][Index]")
   {
-    BufferSource bEncodeSource { "d3:one10:01234567895:three6:qwerty3:two9:asdfghjkle" };
+    BufferSource bEncodeSource{"d3:one10:01234567895:three6:qwerty3:two9:asdfghjkle"};
     bEncode.decode(bEncodeSource);
     REQUIRE(BNodeRef<BNodeString>((*bEncode.getRoot())["one"]).getString() == "0123456789");
     REQUIRE(BNodeRef<BNodeString>((*bEncode.getRoot())["two"]).getString() == "asdfghjkl");
@@ -220,7 +220,7 @@ TEST_CASE("Use of BNode indexing operators", "[Bencode][BNode][Index]")
   }
   SECTION("Decode list and check its components using indexing", "[Bencode][BNode][Index]")
   {
-    BufferSource bEncodeSource { "l6:sillyy12:poiuytrewqas26:abcdefghijklmnopqrstuvwxyze" };
+    BufferSource bEncodeSource{"l6:sillyy12:poiuytrewqas26:abcdefghijklmnopqrstuvwxyze"};
     bEncode.decode(bEncodeSource);
     REQUIRE(BNodeRef<BNodeString>((*bEncode.getRoot())[0]).getString() == "sillyy");
     REQUIRE(BNodeRef<BNodeString>((*bEncode.getRoot())[1]).getString() == "poiuytrewqas");
@@ -228,7 +228,7 @@ TEST_CASE("Use of BNode indexing operators", "[Bencode][BNode][Index]")
   }
   SECTION("Decode list with embedded dictioanry and check its components using indexing", "[Bencode][BNode][Index]")
   {
-    BufferSource bEncodeSource { "l6:sillyyd3:one10:01234567895:three6:qwerty3:two9:asdfghjkle26:abcdefghijklmnopqrstuvwxyze" };
+    BufferSource bEncodeSource{"l6:sillyyd3:one10:01234567895:three6:qwerty3:two9:asdfghjkle26:abcdefghijklmnopqrstuvwxyze"};
     bEncode.decode(bEncodeSource);
     REQUIRE(BNodeRef<BNodeString>((*bEncode.getRoot())[0]).getString() == "sillyy");
     REQUIRE(BNodeRef<BNodeString>((*bEncode.getRoot())[1]["one"]).getString() == "0123456789");
@@ -238,14 +238,14 @@ TEST_CASE("Use of BNode indexing operators", "[Bencode][BNode][Index]")
   }
   SECTION("Decode dictionary and check an invalid key generates exception", "[Bencode][BNode][Index]")
   {
-    BufferSource bEncodeSource { "d3:one10:01234567895:three6:qwerty3:two9:asdfghjkle" };
+    BufferSource bEncodeSource{"d3:one10:01234567895:three6:qwerty3:two9:asdfghjkle"};
     bEncode.decode(bEncodeSource);
     REQUIRE_THROWS_AS((*bEncode.getRoot())["onee"].nodeType == BNodeType::dictionary, std::runtime_error);
     REQUIRE_THROWS_WITH((*bEncode.getRoot())["onee"].nodeType == BNodeType::dictionary, "Invalid key used in dictionary.");
   }
   SECTION("Decode list and check an invalid index generates exception", "[Bencode][BNode][Index]")
   {
-    BufferSource bEncodeSource {"l6:sillyy12:poiuytrewqas26:abcdefghijklmnopqrstuvwxyze" };
+    BufferSource bEncodeSource{"l6:sillyy12:poiuytrewqas26:abcdefghijklmnopqrstuvwxyze"};
     bEncode.decode(bEncodeSource);
     REQUIRE_THROWS_AS((*bEncode.getRoot())[3].nodeType == BNodeType::list, std::runtime_error);
     REQUIRE_THROWS_WITH((*bEncode.getRoot())[3].nodeType == BNodeType::list, "Invalid index used in list.");
@@ -256,26 +256,43 @@ TEST_CASE("Check BNode reference functions work.", "[Bencode][BNode][Reference]"
   Bencode bEncode;
   SECTION("Integer reference.", "[Bencode][BNode][Reference]")
   {
-    BufferSource bEncodeSource { "i45500e" };
+    BufferSource bEncodeSource{"i45500e"};
     bEncode.decode(bEncodeSource);
     REQUIRE(BNodeRef<BNodeInteger>((*bEncode.getRoot())).getInteger() == 45500);
   }
   SECTION("String reference.", "[Bencode][BNode][Reference]")
   {
-    BufferSource bEncodeSource { "10:0123456789" };
+    BufferSource bEncodeSource{"10:0123456789"};
     bEncode.decode(bEncodeSource);
     REQUIRE(BNodeRef<BNodeString>((*bEncode.getRoot())).getString() == "0123456789");
   }
   SECTION("List reference.", "[Bencode][BNode][Reference]")
   {
-    BufferSource bEncodeSource { "l6:sillyy12:poiuytrewqas26:abcdefghijklmnopqrstuvwxyze" };
+    BufferSource bEncodeSource{"l6:sillyy12:poiuytrewqas26:abcdefghijklmnopqrstuvwxyze"};
     bEncode.decode(bEncodeSource);
     REQUIRE(BNodeRef<BNodeList>((*bEncode.getRoot())).size() == 3);
   }
   SECTION("Dictionary reference.", "[Bencode][BNode][Reference]")
   {
-    BufferSource bEncodeSource { "d3:one10:01234567894:four8:123456785:three6:qwerty3:two9:asdfghjkle" };
+    BufferSource bEncodeSource{"d3:one10:01234567894:four8:123456785:three6:qwerty3:two9:asdfghjkle"};
     bEncode.decode(bEncodeSource);
     REQUIRE(BNodeRef<BNodeDict>((*bEncode.getRoot())).size() == 4);
+  }
+}
+TEST_CASE("Check R-Value reference encode/decode.", "[Bencode][BNode][Reference]")
+{
+  Bencode bEncode;
+  SECTION("Encode/Decode with R-Value reference (Buffer).", "[Bencode][BNode][R-Value Reference]")
+  {
+    bEncode.decode(BufferSource{"i45500e"});
+    bEncode.encode(BufferDestination{}); // Does nothing but for completeness
+    REQUIRE(BNodeRef<BNodeInteger>((*bEncode.getRoot())).getInteger() == 45500);
+  }
+  SECTION("Encode/Decode both with R-Value reference (File).", "[Bencode][BNode][R-alue Reference]")
+  {
+    std::filesystem::remove(prefixTestDataPath(kGeneratedTorrentFile));
+    bEncode.decode(FileSource{prefixTestDataPath(kMultiFileTorrent)});
+    bEncode.encode(FileDestination{prefixTestDataPath(kGeneratedTorrentFile)});
+    REQUIRE_FALSE(!compareFiles(prefixTestDataPath(kMultiFileTorrent), prefixTestDataPath(kGeneratedTorrentFile)));
   }
 }
