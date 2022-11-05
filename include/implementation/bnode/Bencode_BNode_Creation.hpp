9b6d0713cd445ec4302e4e9b3408d1b0b4f89242
Rob Tizzard <clockworkengineer@users.noreply.github.com>
2022-11-05 08:46:27 +0000

    remove bencode_bnode.cpp

--- include/implementation/bnode/Bencode_BNode_Creation.hpp
+++ include/implementation/bnode/Bencode_BNode_Creation.hpp
@@ -23,4 +23,24 @@ inline BNode makeString(std::string stringValue) {
 inline BNode makeInteger(int64_t integer) {
   return (BNode{std::make_unique<Integer>(integer)});
 }
+// =====================
+// BNode index overloads
+// =====================
+// List
+inline BNode &BNode::operator[](int index) {
+  return (BRef<List>(*this)[index]);
+}
+inline const BNode &BNode::operator[](int index) const
+{
+  return (BRef<const List>(*this)[index]);
+}
+// Dictionary
+inline BNode &BNode::operator[](const std::string &key) {
+  return (BRef<Dictionary>(*this)[key]);
+}
+inline const BNode &
+BNode::operator[](const std::string &key) const
+{
+  return (BRef<const Dictionary>(*this)[key]);
+}
 } // namespace Bencode_Lib
