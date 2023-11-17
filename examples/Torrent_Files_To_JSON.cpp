829a2478618d8a0ecc7c117339d89f3a66a711de
Rob Tizzard <clockworkengineer@users.noreply.github.com>
2023-11-17 18:55:50 +0000

    Snapshot.

--- examples/Torrent_Files_To_JSON.cpp
+++ examples/Torrent_Files_To_JSON.cpp
@@ -17,11 +17,9 @@
 #include "plog/Initializers/RollingFileInitializer.h"
 #include "plog/Log.h"
 
-namespace fs = std::filesystem;
-
 std::vector<std::string> createTorrentFileList() {
   std::vector<std::string> fileList;
-  for (auto &file : fs::directory_iterator((fs::current_path() / "files"))) {
+  for (auto &file : std::filesystem::directory_iterator((std::filesystem::current_path() / "files"))) {
     if (file.path().extension() == ".torrent") {
       fileList.push_back(file.path().string());
     }
