7896c4966f21a72e33db3f0f562b3ed7df1a49f3
Rob Tizzard <clockworkengineer@users.noreply.github.com>
2023-12-08 15:19:06 +0000

    SNapshot.

--- include/implementation/variants/Bencode_Integer.hpp
+++ include/implementation/variants/Bencode_Integer.hpp
@@ -4,10 +4,10 @@ namespace Bencode_Lib {
 
 struct Integer : Variant {
   explicit Integer(int64_t integer)
-      : Variant(Variant::Type::integer), value(integer) {}
-  [[nodiscard]] int64_t integer() const { return (value); }
+      : Variant(Variant::Type::integer), bNodeInteger(integer) {}
+  [[nodiscard]] int64_t integer() const { return (bNodeInteger); }
 
 private:
-  int64_t value = 0;
+  int64_t bNodeInteger = 0;
 };
 } // namespace Bencode_Lib
