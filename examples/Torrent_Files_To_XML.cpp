36f78a39c8629fa1f6cc60b703af90dd5a8967bd
Rob Tizzard <clockworkengineer@users.noreply.github.com>
2023-12-12 13:32:32 +0000

    Snapshot.

---
+++ examples/Torrent_Files_To_XML.cpp
@@ -0,0 +1,44 @@
+//
+// Program: Torrent_Files_To_XML
+//
+// Description: Use Bencode_Lib to read in torrent file then write
+// it out as JSON using a custom encoder.
+//
+// Dependencies: C++20, Bencode_Lib.
+//
+
+#include <stdexcept>
+
+#include "Utility.hpp"
+#include "Encoder_XML.hpp"
+
+#include "plog/Initializers/RollingFileInitializer.h"
+#include "plog/Log.h"
+
+std::string createJSONFileName(const std::string &torrentFileName) {
+  std::string jsonFilename = torrentFileName;
+  return (
+      jsonFilename.erase(jsonFilename.find(".torrent"), jsonFilename.length()) +
+      ".json");
+}
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char **argv) {
+  try {
+    Bencode_Lib::Bencode bEncode(std::make_unique<Encoder_XML>().release());
+    // Initialise logging.
+    plog::init(plog::debug, "Torrent_Files_To_XML.log");
+    PLOG_INFO << "Torrent_Files_To_XML started ...";
+    PLOG_INFO << Bencode_Lib::Bencode().version();
+    for (const auto &torrentFileName : Utility::createTorrentFileList()) {
+      bEncode.decode(Bencode_Lib::FileSource(torrentFileName));
+      bEncode.encode(
+          Bencode_Lib::FileDestination(createJSONFileName(torrentFileName)));
+      PLOG_INFO << "Created file " << createJSONFileName(torrentFileName)
+                << " from " << torrentFileName;
+    }
+  } catch (const std::exception &ex) {
+    std::cout << "Error Processing Torrent File: [" << ex.what() << "]\n";
+  }
+  PLOG_INFO << "Torrent_Files_To_XML exited.";
+  exit(EXIT_SUCCESS);
+}
