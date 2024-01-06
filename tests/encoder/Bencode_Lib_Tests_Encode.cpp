7ffedbf4360cf1d555c0454c16a81b6034310f59
Rob Tizzard <clockworkengineer@users.noreply.github.com>
2024-01-06 21:33:35 +0000

    Refactors

--- tests/encoder/Bencode_Lib_Tests_Encode.cpp
+++ tests/encoder/Bencode_Lib_Tests_Encode.cpp
@@ -14,57 +14,57 @@ TEST_CASE("Bencode for encode of simple types (integer, string) ",
   const Bencode bEncode;
   SECTION("Encode an integer (266) and check value",
           "[Bencode][Encode][Integer]") {
-    BufferSource bEncodeSource{"i266e"};
-    BufferDestination bEncodeDestination;
-    bEncode.decode(bEncodeSource);
-    bEncode.encode(bEncodeDestination);
-    REQUIRE(bEncodeDestination.toString() == "i266e");
+    BufferSource source{"i266e"};
+    BufferDestination destination;
+    bEncode.decode(source);
+    bEncode.encode(destination);
+    REQUIRE(destination.toString() == "i266e");
   }
   SECTION("Encode an integer (10000) and check value",
           "[Bencode][Encode][Integer]") {
-    BufferSource bEncodeSource{"i10000e"};
-    BufferDestination bEncodeDestination;
-    bEncode.decode(bEncodeSource);
-    bEncode.encode(bEncodeDestination);
-    REQUIRE(bEncodeDestination.toString() == "i10000e");
+    BufferSource source{"i10000e"};
+    BufferDestination destination;
+    bEncode.decode(source);
+    bEncode.encode(destination);
+    REQUIRE(destination.toString() == "i10000e");
   }
   SECTION("Encode an string ('qwertyuiopas') and check its value",
           "[Bencode][Encode][String]") {
-    BufferSource bEncodeSource{"12:qwertyuiopas"};
-    BufferDestination bEncodeDestination;
-    bEncode.decode(bEncodeSource);
-    bEncode.encode(bEncodeDestination);
-    REQUIRE(bEncodeDestination.toString() == "12:qwertyuiopas");
+    BufferSource source{"12:qwertyuiopas"};
+    BufferDestination destination;
+    bEncode.decode(source);
+    bEncode.encode(destination);
+    REQUIRE(destination.toString() == "12:qwertyuiopas");
   }
   SECTION("Encode an string ('abcdefghijklmnopqrstuvwxyz') and check its value",
           "[Bencode][Encode][String]") {
-    BufferSource bEncodeSource{"26:abcdefghijklmnopqrstuvwxyz"};
-    BufferDestination bEncodeDestination;
-    bEncode.decode(bEncodeSource);
-    bEncode.encode(bEncodeDestination);
-    REQUIRE(bEncodeDestination.toString() == "26:abcdefghijklmnopqrstuvwxyz");
+    BufferSource source{"26:abcdefghijklmnopqrstuvwxyz"};
+    BufferDestination destination;
+    bEncode.decode(source);
+    bEncode.encode(destination);
+    REQUIRE(destination.toString() == "26:abcdefghijklmnopqrstuvwxyz");
   }
 }
 TEST_CASE("Bencode for encode of a table of integer test data",
           "[Bencode][Encode][Integer]") {
   const Bencode bEncode;
   auto [expected] = GENERATE(table<std::string>({"i277e", "i32767e"}));
-  BufferSource bEncodeSource(expected);
-  BufferDestination bEncodeDestination;
-  bEncode.decode(bEncodeSource);
-  bEncode.encode(bEncodeDestination);
-  REQUIRE(bEncodeDestination.toString() == expected);
+  BufferSource source(expected);
+  BufferDestination destination;
+  bEncode.decode(source);
+  bEncode.encode(destination);
+  REQUIRE(destination.toString() == expected);
 }
 TEST_CASE("Bencode for encode of a table of string test data",
           "[Bencode][Encode][String]") {
   const Bencode bEncode;
   auto [expected] =
       GENERATE(table<std::string>({"13:qwertyuiopasd", "6:mnbvcx"}));
-  BufferSource bEncodeSource(expected);
-  BufferDestination bEncodeDestination;
-  bEncode.decode(bEncodeSource);
-  bEncode.encode(bEncodeDestination);
-  REQUIRE(bEncodeDestination.toString() == expected);
+  BufferSource source(expected);
+  BufferDestination destination;
+  bEncode.decode(source);
+  bEncode.encode(destination);
+  REQUIRE(destination.toString() == expected);
 }
 TEST_CASE("Bencode for encode of collection types (list, dictionary) ",
           "[Bencode][Encode]") {
@@ -72,11 +72,11 @@ TEST_CASE("Bencode for encode of collection types (list, dictionary) ",
   SECTION("Encode an List of integers('li266ei6780ei88ee') and check value",
           "[Bencode][Encode][List]") {
     std::string expected{"li266ei6780ei88ee"};
-    BufferSource bEncodeSource(expected);
-    BufferDestination bEncodeDestination;
-    bEncode.decode(bEncodeSource);
-    bEncode.encode(bEncodeDestination);
-    REQUIRE(bEncodeDestination.toString() == expected);
+    BufferSource source(expected);
+    BufferDestination destination;
+    bEncode.decode(source);
+    bEncode.encode(destination);
+    REQUIRE(destination.toString() == expected);
   }
   SECTION("Encode an List of strings "
           "('l6:sillyy12:poiuytrewqas26:abcdefghijklmnopqrstuvwxyze') and "
@@ -84,38 +84,38 @@ TEST_CASE("Bencode for encode of collection types (list, dictionary) ",
           "[Bencode][Encode][List]") {
     std::string expected{
         "l6:sillyy12:poiuytrewqas26:abcdefghijklmnopqrstuvwxyze"};
-    BufferSource bEncodeSource(expected);
-    BufferDestination bEncodeDestination;
-    bEncode.decode(bEncodeSource);
-    bEncode.encode(bEncodeDestination);
-    REQUIRE(bEncodeDestination.toString() == expected);
+    BufferSource source(expected);
+    BufferDestination destination;
+    bEncode.decode(source);
+    bEncode.encode(destination);
+    REQUIRE(destination.toString() == expected);
   }
   SECTION("Encode an Dictionary of integers and check value",
           "[Bencode][Encode][Dictionary]") {
     std::string expected{"d3:onei1e5:threei3e3:twoi2ee"};
-    BufferSource bEncodeSource(expected);
-    BufferDestination bEncodeDestination;
-    bEncode.decode(bEncodeSource);
-    bEncode.encode(bEncodeDestination);
-    REQUIRE(bEncodeDestination.toString() == expected);
+    BufferSource source(expected);
+    BufferDestination destination;
+    bEncode.decode(source);
+    bEncode.encode(destination);
+    REQUIRE(destination.toString() == expected);
   }
   SECTION("Encode an Dictionary of strings and check value",
           "[Bencode][Encode][Dictionary]") {
     std::string expected{"d3:one10:01234567895:three6:qwerty3:two9:asdfghjkle"};
-    BufferSource bEncodeSource(expected);
-    BufferDestination bEncodeDestination;
-    bEncode.decode(bEncodeSource);
-    bEncode.encode(bEncodeDestination);
-    REQUIRE(bEncodeDestination.toString() == expected);
+    BufferSource source(expected);
+    BufferDestination destination;
+    bEncode.decode(source);
+    bEncode.encode(destination);
+    REQUIRE(destination.toString() == expected);
   }
 }
 TEST_CASE("Encode generated exceptions", "[Bencode][Encode][Exceptions]") {
   const Bencode bEncode;
   SECTION("Encode called with no BNode tree to encode",
           "[Bencode][Encode][Exceptions]") {
-    BufferDestination bEncodeDestination;
-    REQUIRE_THROWS_AS(bEncode.encode(bEncodeDestination), Bencode::Error);
-    REQUIRE_THROWS_WITH(bEncode.encode(bEncodeDestination),
+    BufferDestination destination;
+    REQUIRE_THROWS_AS(bEncode.encode(destination), Bencode::Error);
+    REQUIRE_THROWS_WITH(bEncode.encode(destination),
                         "Bencode Error: No Bencoded data to encode.");
   }
 }
@@ -125,22 +125,22 @@ TEST_CASE("Encode torrent files using encodeToFile",
   SECTION("Encode singlefile.torrent and check value",
           "[Bencode][Encode][Torrents]") {
     std::filesystem::remove(prefixTestDataPath(kGeneratedTorrentFile));
-    FileSource bEncodeSource{prefixTestDataPath(kSingleFileTorrent)};
-    FileDestination bEncodeDestination{
+    FileSource source{prefixTestDataPath(kSingleFileTorrent)};
+    FileDestination destination{
         prefixTestDataPath(kGeneratedTorrentFile)};
-    bEncode.decode(bEncodeSource);
-    bEncode.encode(bEncodeDestination);
+    bEncode.decode(source);
+    bEncode.encode(destination);
     REQUIRE_FALSE(!compareFiles(prefixTestDataPath(kSingleFileTorrent),
                                 prefixTestDataPath(kGeneratedTorrentFile)));
   }
   SECTION("Encode multifile.torrent and check value",
           "[Bencode][Encode][Torrents]") {
     std::filesystem::remove(prefixTestDataPath(kGeneratedTorrentFile));
-    FileSource bEncodeSource{prefixTestDataPath(kMultiFileTorrent)};
-    FileDestination bEncodeDestination{
+    FileSource source{prefixTestDataPath(kMultiFileTorrent)};
+    FileDestination destination{
         prefixTestDataPath(kGeneratedTorrentFile)};
-    bEncode.decode(bEncodeSource);
-    bEncode.encode(bEncodeDestination);
+    bEncode.decode(source);
+    bEncode.encode(destination);
     REQUIRE_FALSE(!compareFiles(prefixTestDataPath(kMultiFileTorrent),
                                 prefixTestDataPath(kGeneratedTorrentFile)));
   }
--- tests/encoder/Bencode_Lib_Tests_JSON_Encode.cpp
+++ tests/encoder/Bencode_Lib_Tests_JSON_Encode.cpp
@@ -14,36 +14,36 @@ TEST_CASE("JSON encode of simple types (integer, string) ",
   const Bencode bEncode(std::make_unique<JSON_Encoder>().release());
   SECTION("JSON encode an integer (266) and check value",
           "[Bencode][Encode][JSON][Integer]") {
-    BufferSource bEncodeSource{"i266e"};
-    BufferDestination bEncodeDestination;
-    bEncode.decode(bEncodeSource);
-    bEncode.encode(bEncodeDestination);
-    REQUIRE(bEncodeDestination.toString() == "266");
+    BufferSource source{"i266e"};
+    BufferDestination destination;
+    bEncode.decode(source);
+    bEncode.encode(destination);
+    REQUIRE(destination.toString() == "266");
   }
   SECTION("JSON encode an integer (10000) and check value",
           "[Bencode][Encode][JSON][Integer]") {
-    BufferSource bEncodeSource{"i10000e"};
-    BufferDestination bEncodeDestination;
-    bEncode.decode(bEncodeSource);
-    bEncode.encode(bEncodeDestination);
-    REQUIRE(bEncodeDestination.toString() == "10000");
+    BufferSource source{"i10000e"};
+    BufferDestination destination;
+    bEncode.decode(source);
+    bEncode.encode(destination);
+    REQUIRE(destination.toString() == "10000");
   }
   SECTION("JSON encode an string ('qwertyuiopas') and check its value",
           "[Bencode][Encode][JSON][String]") {
-    BufferSource bEncodeSource{"12:qwertyuiopas"};
-    BufferDestination bEncodeDestination;
-    bEncode.decode(bEncodeSource);
-    bEncode.encode(bEncodeDestination);
-    REQUIRE(bEncodeDestination.toString() == R"("qwertyuiopas")");
+    BufferSource source{"12:qwertyuiopas"};
+    BufferDestination destination;
+    bEncode.decode(source);
+    bEncode.encode(destination);
+    REQUIRE(destination.toString() == R"("qwertyuiopas")");
   }
   SECTION("JSON encode an string ('abcdefghijklmnopqrstuvwxyz') and check its "
           "value ",
           "[Bencode][Encode][JSON][String]") {
-    BufferSource bEncodeSource{"26:abcdefghijklmnopqrstuvwxyz"};
-    BufferDestination bEncodeDestination;
-    bEncode.decode(bEncodeSource);
-    bEncode.encode(bEncodeDestination);
-    REQUIRE(bEncodeDestination.toString() == R"("abcdefghijklmnopqrstuvwxyz")");
+    BufferSource source{"26:abcdefghijklmnopqrstuvwxyz"};
+    BufferDestination destination;
+    bEncode.decode(source);
+    bEncode.encode(destination);
+    REQUIRE(destination.toString() == R"("abcdefghijklmnopqrstuvwxyz")");
   }
 }
 TEST_CASE("JSON encode of collection types (list, dictionary) ",
@@ -52,51 +52,46 @@ TEST_CASE("JSON encode of collection types (list, dictionary) ",
   SECTION(
       "JSON encode an List of integers('li266ei6780ei88ee') and check value",
       "[Bencode][Encode][JSON][List]") {
-    std::string expected{"li266ei6780ei88ee"};
-    BufferSource bEncodeSource(expected);
-    BufferDestination bEncodeDestination;
-    bEncode.decode(bEncodeSource);
-    bEncode.encode(bEncodeDestination);
-    REQUIRE(bEncodeDestination.toString() == R"([266,6780,88])");
+    BufferSource source{"li266ei6780ei88ee"};
+    BufferDestination destination;
+    bEncode.decode(source);
+    bEncode.encode(destination);
+    REQUIRE(destination.toString() == R"([266,6780,88])");
   }
   SECTION("JSON encode an List of integers and strings "
           "('li266e4:fivei6780e3:onei88ee') and check value",
           "[Bencode][Encode][JSON][List]") {
-    std::string expected{"li266e4:fivei6780e3:onei88ee"};
-    BufferSource bEncodeSource(expected);
-    BufferDestination bEncodeDestination;
-    bEncode.decode(bEncodeSource);
-    bEncode.encode(bEncodeDestination);
-    REQUIRE(bEncodeDestination.toString() == R"([266,"five",6780,"one",88])");
+    BufferSource source{"li266e4:fivei6780e3:onei88ee"};
+    BufferDestination destination;
+    bEncode.decode(source);
+    bEncode.encode(destination);
+    REQUIRE(destination.toString() == R"([266,"five",6780,"one",88])");
   }
   SECTION("JSON encode an Dictionary of integers and check value",
           "[Bencode][Encode][JSON][Dictionary]") {
-    std::string expected{"d3:onei1e5:threei3e3:twoi2ee"};
-    BufferSource bEncodeSource(expected);
-    BufferDestination bEncodeDestination;
-    bEncode.decode(bEncodeSource);
-    bEncode.encode(bEncodeDestination);
-    REQUIRE(bEncodeDestination.toString() ==
+    BufferSource source{"d3:onei1e5:threei3e3:twoi2ee"};
+    BufferDestination destination;
+    bEncode.decode(source);
+    bEncode.encode(destination);
+    REQUIRE(destination.toString() ==
             R"({"one" : 1,"three" : 3,"two" : 2})");
   }
   SECTION("JSON encode an Dictionary of strings and check value",
           "[Bencode][Encode][JSON][Dictionary]") {
-    std::string expected{"d3:one10:01234567895:three6:qwerty3:two9:asdfghjkle"};
-    BufferSource bEncodeSource(expected);
-    BufferDestination bEncodeDestination;
-    bEncode.decode(bEncodeSource);
-    bEncode.encode(bEncodeDestination);
-    REQUIRE(bEncodeDestination.toString() ==
+    BufferSource source{"d3:one10:01234567895:three6:qwerty3:two9:asdfghjkle"};
+    BufferDestination destination;
+    bEncode.decode(source);
+    bEncode.encode(destination);
+    REQUIRE(destination.toString() ==
             R"({"one" : "0123456789","three" : "qwerty","two" : "asdfghjkl"})");
   }
   SECTION("JSON encode an Dictionary of arrays and check value",
           "[Bencode][Encode][JSON][Dictionary]") {
-    std::string expected{"d3:oneli1ei2ei3ee3:twoli5555e4:fouree"};
-    BufferSource bEncodeSource(expected);
-    BufferDestination bEncodeDestination;
-    bEncode.decode(bEncodeSource);
-    bEncode.encode(bEncodeDestination);
-    REQUIRE(bEncodeDestination.toString() ==
+    BufferSource source{"d3:oneli1ei2ei3ee3:twoli5555e4:fouree"};
+    BufferDestination destination;
+    bEncode.decode(source);
+    bEncode.encode(destination);
+    REQUIRE(destination.toString() ==
             R"({"one" : [1,2,3],"two" : [5555,"four"]})");
   }
 }
