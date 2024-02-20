f0d38bf01fa6c6346420b161e6d415f9962638ae
Rob Tizzard <clockworkengineer@users.noreply.github.com>
2024-02-20 18:07:41 +0000

    Add ITranslate.

---
+++ include/interface/ITranslator.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <string>
+#include <stdexcept>
+#include <array>
+
+namespace Bencode_Lib {
+
+// ====================================================
+// Interface for translation to/from escaped characters
+// ====================================================
+class ITranslator
+{
+public:
+  // =============
+  // ITranslator Error
+  // =============
+  struct Error : public std::runtime_error
+  {
+    explicit Error(const std::string &message) : std::runtime_error("ITranslator Error: " + message) {}
+  };
+  // ========================
+  // Constructors/destructors
+  // ========================
+  virtual ~ITranslator() = default;
+  // =====================================================================
+  // Convert any escape sequences in a string to their correct sequence
+  // of UTF-8 characters.
+  // =====================================================================
+  virtual std::string from(const std::string &escapedString) = 0;
+  // =========================================================================
+  // Convert a string from raw character values (UTF8) so that it has character
+  // escapes where applicable for its form.
+  // =========================================================================
+  virtual std::string to(const std::string &rawString) = 0;
+};
+}// namespace Bencode_Lib
