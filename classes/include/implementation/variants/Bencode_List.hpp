94020f8e7f9229b19fff30587692699ac1ad5678
clockworkengineer <robert_tizzard@hotmail.com>
2024-06-24 16:39:44 +0100

    Refactor.

--- classes/include/implementation/variants/Bencode_List.hpp
+++ classes/include/implementation/variants/Bencode_List.hpp
@@ -18,8 +18,9 @@ struct List : Variant {
   List &operator=(List &&other) = default;
   ~List() = default;
   // Add array element
-  void add(BNode &bNode) { bNodeList.emplace_back(std::move(bNode)); }
-  void add(BNode &&bNode) { bNodeList.emplace_back(std::move(bNode)); }
+  void add(BNode bNode) {
+    bNodeList.emplace_back(std::move(bNode));
+  }
   // Get BNode size
   [[nodiscard]] int size() const {
     return static_cast<int>(bNodeList.size());
@@ -27,17 +28,14 @@ struct List : Variant {
   // Get BNode value
   [[nodiscard]] std::vector<BNode> &value() { return bNodeList; }
   [[nodiscard]] const std::vector<BNode> &value() const { return bNodeList; }
+  // Get BNode at index
   BNode &operator[](const int index) {
-    if (index >= 0 && index < static_cast<int>(bNodeList.size())) {
-      return bNodeList[index];
-    }
-    throw Error("Invalid index used in list.");
+    validateIndex(index);
+    return bNodeList[index];
   }
   const BNode &operator[](const int index) const {
-    if (index >= 0 && index < static_cast<int>(bNodeList.size())) {
-      return bNodeList[index];
-    }
-    throw Error("Invalid index used in list.");
+    validateIndex(index);
+    return bNodeList[index];
   }
   // Resize Array
   void resize(const std::size_t index) {
@@ -50,6 +48,13 @@ struct List : Variant {
   }
 
 private:
+  // ensure the index is within the bounds
+  void validateIndex(int index) const {
+    if (!(index >= 0 && index < static_cast<int>(bNodeList.size()))) {
+      throw Error("Invalid index used in list.");
+    }
+  }
+
   std::vector<BNode> bNodeList;
 };
 } // namespace Bencode_Lib
