98ee589a7b9bff66a014098d8c9bfe8d5afee3be
clockworkengineer <robert_tizzard@hotmail.com>
2024-06-30 20:57:00 +0100

    refactor.

--- classes/include/implementation/bnode/Bencode_BNode_Reference.hpp
+++ classes/include/implementation/bnode/Bencode_BNode_Reference.hpp
@@ -2,9 +2,9 @@
 
 namespace Bencode_Lib {
 
-// =====================
-// What is BNode variant
-// =====================
+// =======================
+// What is BNode variant ?
+// =======================
 template <typename T> bool isA(const BNode &bNode) {
   if constexpr (std::is_same_v<T, String>) {
     return bNode.getVariant().getNodeType() == Variant::Type::string;
