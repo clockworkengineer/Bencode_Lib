37f2eebc5a2d798ec0b9b13e0d25e4de2e88b69c
clockworkengineer <robert_tizzard@hotmail.com>
2024-07-01 14:54:43 +0100

    Refactor

--- classes/include/implementation/bnode/Bencode_BNode_Reference.hpp
+++ classes/include/implementation/bnode/Bencode_BNode_Reference.hpp
@@ -16,6 +16,8 @@ template <typename T> bool isA(const BNode &bNode) {
     return bNode.getVariant().getNodeType() == Variant::Type::dictionary;
   } else if constexpr (std::is_same_v<T, Hole>) {
     return bNode.getVariant().getNodeType() == Variant::Type::hole;
+  } else {
+    static_assert(false, "Not a valid BNode variant.");
   }
 }
 // ==============================
@@ -23,23 +25,23 @@ template <typename T> bool isA(const BNode &bNode) {
 // ==============================
 template <typename T> void CheckBNodeType(const BNode &bNode) {
   if constexpr (std::is_same_v<T, String>) {
-    if (!isA<String>(bNode)) {
+    if (!isA<T>(bNode)) {
       throw BNode::Error("Node not a string.");
     }
   } else if constexpr (std::is_same_v<T, Integer>) {
-    if (!isA<Integer>(bNode)) {
+    if (!isA<T>(bNode)) {
       throw BNode::Error("Node not an integer.");
     }
   } else if constexpr (std::is_same_v<T, List>) {
-    if (!isA<List>(bNode)) {
+    if (!isA<T>(bNode)) {
       throw BNode::Error("Node not a list.");
     }
   } else if constexpr (std::is_same_v<T, Dictionary>) {
-    if (!isA<Dictionary>(bNode)) {
+    if (!isA<T>(bNode)) {
       throw BNode::Error("Node not a dictionary.");
     }
   } else if constexpr (std::is_same_v<T, Hole>) {
-    if (!isA<Hole>(bNode)) {
+    if (!isA<T>(bNode)) {
       throw BNode::Error("Node not a hole.");
     }
   }
